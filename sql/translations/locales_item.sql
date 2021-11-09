b92dad07a6bdd2e9de9e51cd75fea326bd3cda1a
gogolxdong <lxdong.007@gmail.com>
2021-11-09 08:49:12 +0800

    add locales_item and quest

---
+++ sql/translations/locales_item.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fa46340a94f9ab33dbe1753b8f7e38b2488d28b722340b842c4090b167e80792
+size 4057669
---
+++ sql/translations/locales_quest.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9a78172eb4271782255cd041e223a3439b3eff7c1c93e1f3bcc7e557f214a2ea
+size 20605626
